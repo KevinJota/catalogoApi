7aa4fdcf5bddcbb9c28a8508d250961919cb8946
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-05-13 21:27:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+// Importa os módulos necessários
+const express = require('express'); // Importa o módulo Express.js para criar o servidor
+const mongoose = require('mongoose'); // Importa o módulo Mongoose para interagir com o MongoDB
+const cors = require('cors'); // Importa o módulo CORS para habilitar o acesso a recursos de diferentes origens
 
+// Cria e inicializa o aplicativo Express
 const app = express();
 app.use(express.json());
 
 // isso Permite solicitações de qualquer origem
 app.use(cors());
 
+// aqui é definido a porta
 const port = 3002;
 
 // criando collection/tabela do banco catalogo
@@ -18,20 +21,23 @@ const Jogo = mongoose.model('Jogo',{
     trailer_url : String,
     ano_Lancamento :String,
     plataforma : String
-});
+}); // isso aq Define o esquema de dados para os jogos
 
-// método para consultar dados de algum jogo
+// método para consultar dados de todos os jogos
 app .get('/', async(req, res) =>{
     const jogos = await Jogo.find();
     return res.send(jogos);
 }),
 
+// método para consultar um jogo pelo seu ID
 app.get('/:_id', async (req, res) => {
     const _id = req.params._id; // Obtém o título diretamente do caminho da URL
     const jogos = await Jogo.find({ _id: _id }); // Busca os jogos pelo título
     return res.send(jogos);
   }),
 
+// método de consulta multiplas de jogos por titulos, ele irá retornar jogos
+// que estiverem o titulo mais de acordo com o parametro recebido
   app.get('/titulo/:title', async (req, res) => {
     console.log("Buscando por jogo:", req.params.title);
     const keyword = req.params.title;
@@ -40,13 +46,14 @@ app.get('/:_id', async (req, res) => {
     return res.send(jogos);
   });
 
+// método de consulta multiplas de jogos por Ano de lançamento dos jogos
   app.get('/ano/:ano_Lancamento', async (req, res) => {
     const ano_Lancamento = req.params.ano_Lancamento; // Obtém o título diretamente do caminho da URL
     const jogos = await Jogo.find({ ano_Lancamento: ano_Lancamento }); // Busca os jogos pelo título
     return res.send(jogos);
   }),
 
-//método utilizado para Adicionar novos dados de jogos(uso interno)
+// método utilizado para Adicionar novos dados de jogos(uso interno)
 app.post('/', async(req, res) =>{
     const jogo = new Jogo({
         title: req.body.title,
